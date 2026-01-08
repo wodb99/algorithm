9982c02dc7e6c0dd60777005c6720f01f5da77cf
wodb99 <rlawodb2405@naver.com>
2026-01-08 17:17:31 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = []
+    a, b = 0, 0
+    for i in num_list:
+        if i % 2 == 0:
+            a += 1
+        else:
+            b += 1
+    answer.append(a)
+    answer.append(b)
+    return answer
