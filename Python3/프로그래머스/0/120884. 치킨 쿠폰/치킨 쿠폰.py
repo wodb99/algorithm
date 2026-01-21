8209c0c339d103bf243a7da8300502fe5eda700e
wodb99 <rlawodb2405@naver.com>
2026-01-21 17:00:20 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 8.84 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,10 @@
+def solution(chicken):
+    answer = 0
+    
+    while chicken >= 10:
+        a = chicken // 10
+        b = chicken % 10
+        answer += a
+        chicken = a + b
+            
+    return answer
