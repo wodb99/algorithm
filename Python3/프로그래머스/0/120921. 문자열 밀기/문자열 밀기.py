12618cfd9dc10518e392a64f4cc7f8214f676aeb
wodb99 <rlawodb2405@naver.com>
2026-01-24 16:55:21 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 9.13 MB -BaekjoonHub

--- Python3/프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ Python3/프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -3,7 +3,7 @@ def solution(A, B):
         return 0
     
     temp = A
-    for cnt in range(1, len(A) + 1):
+    for cnt in range(1, len(A)):
         temp = temp[-1] + temp[:-1]
         if temp == B:
             return cnt
