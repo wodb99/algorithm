d6f9add4fa4640d079dc783f0043dbd5070af1d4
wodb99 <rlawodb2405@naver.com>
2026-01-24 17:05:44 +0900

    Refactor string shifting solution using deque

--- Python3/프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ Python3/프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,11 +1,23 @@
-def solution(A, B):
-    if A == B:
-        return 0
+# def solution(A, B):
+#     if A == B:
+#         return 0
     
-    temp = A
-    for cnt in range(1, len(A)):
-        temp = temp[-1] + temp[:-1]
-        if temp == B:
-            return cnt
+#     temp = A
+#     for cnt in range(1, len(A)):
+#         temp = temp[-1] + temp[:-1]
+#         if temp == B:
+#             return cnt
             
+#     return -1
+
+from collections import deque
+
+def solution(A, B):
+    a, b = deque(A), deque(B)
+    
+    for cnt in range(0, len(A)):
+        if a == b:
+            return cnt
+        a.rotate(1)
+    
     return -1
