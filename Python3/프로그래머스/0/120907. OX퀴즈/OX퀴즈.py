e13f65ce932cbe2e55f6e6812f1c9e096a8cf6c4
wodb99 <rlawodb2405@naver.com>
2026-01-30 22:13:45 +0900

    [level 0] Title: OX퀴즈, Time: 0.05 ms, Memory: 9.41 MB -BaekjoonHub

--- Python3/프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ Python3/프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -1,8 +1,30 @@
+# def solution(quiz):
+#     answer = []
+#     for i in quiz:
+#         if eval(i.split('=')[0]) == int(i.split('=')[1]):
+#             answer.append('O')
+#         else:
+#             answer.append('X')
+#     return answer
+
 def solution(quiz):
     answer = []
+    
     for i in quiz:
-        if eval(i.split('=')[0]) == int(i.split('=')[1]):
+        left, right = i.split('=')
+        right = int(right)
+        
+        a, op, b = left.split()
+        a, b = int(a), int(b)
+        
+        if op == '+':
+            result = a + b
+        else:
+            result = a - b
+            
+        if result == right:
             answer.append('O')
         else:
             answer.append('X')
+            
     return answer
