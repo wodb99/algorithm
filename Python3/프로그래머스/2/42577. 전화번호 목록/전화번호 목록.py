097beb9fa696f45b569db264ea01b2f6e12429c2
wodb99 <rlawodb2405@naver.com>
2026-02-07 11:36:43 +0900

    [level 2] Title: 전화번호 목록, Time: 403.71 ms, Memory: 40.6 MB -BaekjoonHub

--- Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,12 @@
 def solution(phone_book):
-    phone_book.sort()
-    
-    for i in range(len(phone_book) - 1):
-        if phone_book[i + 1].startswith(phone_book[i]):
-            return False
-        
+    phone_set = set(phone_book)
+
+    for number in phone_book:
+        temp = ""
+        for ch in number:
+            temp += ch
+            
+            if temp in phone_set and temp != number:
+                return False
+
     return True
