7827fabf1e185e35b2a10ac77ceecc760fdcdb1e
wodb99 <rlawodb2405@naver.com>
2026-09-18 12:37:09 +0900

    [level 2] Title: 주식가격, Time: 14.83 ms, Memory: 17.2 MB -BaekjoonHub

--- Python/프로그래머스/2/42584. 주식가격/주식가격.py
+++ Python/프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,37 +1,16 @@
-from collections import deque
-
 def solution(prices):
-    queue = deque(prices)
-    answer = []
+    answer = [0] * len(prices)
+    stack = []
     
-    while queue:
-        price = queue.popleft()
-        sec = 0
-        for q in queue:
-            sec += 1
-            if price > q:
-                break
-        answer.append(sec)
+    for i in range(len(prices)):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+        
+        stack.append(i)
+    
+    while stack:
+        j = stack.pop()
+        answer[j] = len(prices) - 1 - j
+        
     return answer
-
-# 제대로 이해안가지만 시간 복잡도 더 좋음
-# def solution(prices):
-#     n = len(prices)
-#     answer = [0] * n
-#     stack = []
-    
-#     for i in range(n):
-        
-#         # 현재 가격이 더 작으면 → 떨어진 것
-#         while stack and prices[stack[-1]] > prices[i]:
-#             top = stack.pop()
-#             answer[top] = i - top
-        
-#         stack.append(i)
-    
-#     # 끝까지 안 떨어진 애들 처리
-#     while stack:
-#         top = stack.pop()
-#         answer[top] = n - 1 - top
-        
-#     return answer
