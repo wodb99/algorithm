53f2641ac7d5a255a44f0700c612b75f7b0cce91
wodb99 <rlawodb2405@naver.com>
2026-09-18 11:59:04 +0900

    [level 2] Title: 프로세스, Time: 1.24 ms, Memory: 11.2 MB -BaekjoonHub

--- Python/프로그래머스/2/42587. 프로세스/프로세스.py
+++ Python/프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,16 +1,14 @@
 from collections import deque
 
 def solution(priorities, location):
-    answer = []
-    queue = deque((i, j) for i, j in enumerate(priorities)) # 인덱스와 같이 저장
+    q = deque((priority, idx) for idx, priority in enumerate(priorities))
+    answer = 0
     
-    while queue:
-        process = queue.popleft()
-        if queue and any(process[1] < q[1] for q in queue): # 우선순위가 큰 게 하나라도 존재한다면 다시 큐에 집어넣음
-            queue.append(process)
+    while q:
+        current = q.popleft()
+        if q and current[0] < max(x[0] for x in q):
+            q.append(current)
         else:
-            answer.append(process)
-        
-    for i in answer:
-        if i[0] == location:
-            return answer.index(i) + 1
+            answer += 1
+            if current[1] == location:
+                return answer
